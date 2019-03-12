50c9c17159c141944f9d96e1ad69f042066fba92
aspidee <gabrielemandara@gmail.com>
2019-03-12 02:02:53 +0100

    js start

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// funzione che aggiunge un nuovo messaggio
+function addNewMessage() {
+  $(".new-message-inputs").keyup(function(e) {
+
+    if (e.which == 13) {
+
+      var wrapper = $(".wrapper_right_msg");
+
+      var message = document.createElement("div");
+      var messageContent = document.createElement("p");
+      var messageDetail = document.createElement("span");
+
+      $(message).addClass("message");
+      $(message).addClass("received");
+
+      messageContent = $(".new-message-inputs").val();
+      console.log(messageContent);
+
+      $(messageContent).text(messageContent);
+      $(messageDetail).text("00:00");
+
+      message.append(messageContent);
+      message.append(messageDetail);
+
+      wrapper.append(message);
+
+    }
+  })
+}
+
+
+function init() {
+  addNewMessage();
+}
+
+$(document).ready(init);
+
+
 // function testAddMessage1() {
 //
 //   var wrapper = $(".wrapper");
@@ -38,16 +76,10 @@
 //   wrapper.append(message);
 // }
 //
+// function init() {
+//   var btn1 = $("#myBtn1");
+//   btn1.click(testAddMessage1);
 //
-//
-
-
-function init() {
-  var btn1 = $("#myBtn1");
-  btn1.click(testAddMessage1);
-
-  var btn2 = $("#myBtn2");
-  btn2.click(testAddMessage2);
-}
-
-$(document).ready(init);
+//   var btn2 = $("#myBtn2");
+//   btn2.click(testAddMessage2);
+// }
