2e4e72b8be6fe3f0bbbeade7fde2e99d07185775
aspidee <gabrielemandara@gmail.com>
2019-03-12 13:04:32 +0100

    milestone 1 e 2 completed

--- script.js
+++ script.js
@@ -5,81 +5,110 @@ function addNewMessage() {
     if (e.which == 13) {
 
       var wrapper = $(".wrapper_right_msg");
-
+      var date = new Date();
       var message = document.createElement("div");
       var messageContent = document.createElement("p");
       var messageDetail = document.createElement("span");
+      var messageOptions = document.createElement("i");
 
       $(message).addClass("message");
       $(message).addClass("received");
+      $(messageOptions).addClass("fas fa-angle-down");
 
-      messageContent = $(".new-message-inputs").val();
-      console.log(messageContent);
+      newMessageContent = $(".new-message-inputs").val();
+      console.log(newMessageContent);
 
-      $(messageContent).text(messageContent);
-      $(messageDetail).text("00:00");
+      $(messageContent).text(newMessageContent);
+      $(messageDetail).text(date.getHours() + ":" + date.getMinutes());
 
       message.append(messageContent);
       message.append(messageDetail);
-
+      message.append(messageOptions);
       wrapper.append(message);
 
+      // al trascorrere del timeout lancio la funzione autoReply
+      // e cancello i valori dall'input
+      setTimeout(autoReply, 2000);
+      $(".new-message-inputs").val("");
     }
   })
 }
 
+// funzione che restituisce una risposta automatica
+// all'inserimento di un nuovo messaggio
+function autoReply() {
+
+  var wrapper = $(".wrapper_right_msg");
+  var date = new Date();
+  var message = document.createElement("div");
+  var messageContent = document.createElement("p");
+  var messageDetail = document.createElement("span");
+  var messageOptions = document.createElement("i");
+
+  $(message).addClass("message");
+  $(message).addClass("sent");
+  $(messageOptions).addClass("fas fa-angle-down");
+
+
+  $(messageContent).text("Messaggio ricevuto correttamente");
+  $(messageDetail).text(date.getHours() + ":" + date.getMinutes());
+
+  message.append(messageContent);
+  message.append(messageDetail);
+  message.append(messageOptions);
+  wrapper.append(message);
+}
+
+
+// funzione che ricerca un contatto
+function search() {
+
+  var me = $("#chat_input");
+  var content = me.val().toLowerCase();
+
+  var list = $(".wrapper_left_chat_user");
+
+  list.removeClass("hidden");
+
+  for (var i = 0; i < list.length; i++) {
+
+    var contact = list.eq(i);
+    var name = contact.find("h3");
+    var listContent = name.text().toLowerCase();
+
+    if (!listContent.includes(content)) {
+
+      contact.addClass("hidden");
+    }
+  }
+}
+
+
 
 function init() {
   addNewMessage();
+
+  var input =$("#chat_input");
+  input.keyup(search);
 }
 
 $(document).ready(init);
 
 
-// function testAddMessage1() {
+
+
+
+
+// MILESTONES
 //
-//   var wrapper = $(".wrapper");
+// Milestone 1 - 11/03/19
+// Replica della grafica
+// Aggiunta di un messaggio
 //
-//   var message = document.createElement("div");
-//   var messageContent = document.createElement("p");
-//   var messageDetail = document.createElement("small");
+// Milestone 2 - 12/03/19
+// Risposta dell'interlocutore "Ok" dopo un secondo
+// Ricerca di utenti
 //
-//   $(message).addClass("message");
-//   $(message).addClass("sent");
-//
-//   $(messageContent).text("messaggio dinamico");
-//   $(messageDetail).text("dettagli messaggio dinamico");
-//
-//   message.append(messageContent);
-//   message.append(messageDetail);
-//
-//   wrapper.append(message);
-// }
-//
-// function testAddMessage2() {
-//
-//   var wrapper = $(".wrapper");
-//
-//   var message = document.createElement("div");
-//   var messageContent = document.createElement("p");
-//   var messageDetail = document.createElement("small");
-//
-//   $(message).addClass("message");
-//   $(message).addClass("received");
-//
-//   $(messageContent).text("messaggio dinamico");
-//   $(messageDetail).text("dettagli messaggio dinamico");
-//
-//   message.append(messageContent);
-//   message.append(messageDetail);
-//
-//   wrapper.append(message);
-// }
-//
-// function init() {
-//   var btn1 = $("#myBtn1");
-//   btn1.click(testAddMessage1);
-//
-//   var btn2 = $("#myBtn2");
-//   btn2.click(testAddMessage2);
-// }
+// Milestone 3 - 13/03/19
+// Click sul contatto
+// Cancella messaggio
